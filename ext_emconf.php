b290b9a0e1c3f3789316f0d830b0fab68ab49be1
Hacksch <hacksch@rautemusik.fm>
2016-06-17 18:33:58 +0200

    add extension files add portfolio template files add TYPO3 configuration

---
+++ ext_emconf.php
@@ -0,0 +1,30 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Material Design Lite Portfolio Template',
+  'description' => 'Google Material Design Lite portfolio template implementation for TYPO3',
+  'category' => 'fe',
+  'version' => '1.0.0',
+  'state' => 'stable',
+  'createDirs' => '',
+  'clearcacheonload' => false,
+  'author' => 'Martin Tepper',
+  'author_email' => 'martintepper@dev-mt.de',
+  'author_company' => '',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '6.2.0-8.99.99',
+      'material_design_lite' => '2.0.0'
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'uploadfolder' => false,
+);
+
